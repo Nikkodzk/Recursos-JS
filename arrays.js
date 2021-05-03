f1e98b89812c3bb471b167727214127011f48a49
Nicolas <60112394+Nikkodzk@users.noreply.github.com>
2021-05-03 17:14:24 -0300

    Create arrays.js

---
+++ arrays.js
@@ -0,0 +1,7 @@
+push() => +1 al final
+pop() => -1 al final
+
+shift() => -1 al inicio
+unshift() => +1 al inicio
+
+splice(0,2,'algo','algomas') => remueve desde la posicion 0, 2 elementos y agrega desde la posicion 0 los elementos 'algo ' y 'algo mas'
