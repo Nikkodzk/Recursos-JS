5f3a49b6c41df5e8ab1d36fb8d37767bc475e0d2
Nicolas <60112394+Nikkodzk@users.noreply.github.com>
2021-05-25 11:21:39 -0300

    Update arrays.js

--- arrays.js
+++ arrays.js
@@ -5,3 +5,10 @@ shift() => -1 al inicio
 unshift() => +1 al inicio
 
 splice(0,2,'algo','algomas') => remueve desde la posicion 0, 2 elementos y agrega desde la posicion 0 los elementos 'algo ' y 'algo mas'
+
+
+------------- forEach() -------------
+
+arr.forEach((e, index) => {
+  console.log(e,index);
+})
