e8f3cea05aff96601e23eccaa9fad21c4d86427e
Nicolas <60112394+Nikkodzk@users.noreply.github.com>
2021-05-27 20:10:29 -0300

    Update arrays.js

--- arrays.js
+++ arrays.js
@@ -12,3 +12,13 @@ splice(0,2,'algo','algomas') => remueve desde la posicion 0, 2 elementos y agreg
 arr.forEach((e, index) => {
   console.log(e,index);
 })
+
+------------- map() -------------
+
+  - Mantiene el array original sin modificar
+  - Devuelve un nuevo array de la MISMA longitud que el array inicial
+  
+let numeros = [1,2,3,4,5,6,7,8,9,0];
+let numerosPorDos = numeros.map(x => x*2 );
+console.log(numerosPorDos)  // [2,4,6,8,10,12,14,16,18,0]
+
